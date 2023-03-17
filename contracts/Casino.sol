8a4261bdf43b21602fa394979ce516438857054e
Jason Graves <jgraves@datto.com>
2023-03-17 17:48:57 -0600

    Updated with challenge, adapted tests

--- contracts/Casino.sol
+++ contracts/Casino.sol
@@ -15,12 +15,17 @@ contract Casino {
         uint value;
         uint placedAt;
         bool accepted;
+        uint randomA;
+        uint hashA;
+        bool reveal;
     } // struct ProposedBet
 
     struct AcceptedBet {
         address sideB;
         uint acceptedAt;
         uint randomB;
+        uint hashB;
+        bool reveal;
     } // struct AcceptedBet
 
     // Proposed bets, keyed by the commitment value
@@ -40,47 +45,142 @@ contract Casino {
         uint value
     );
 
+    event FraudDetected(uint indexed _commitment, address _address);
+
+    event BetRevealed(uint indexed _commitment, address indexed _address);
+
     // Called by sideA to start the process
-    function proposeBet(uint _commitment) external payable {
+    function proposeBet(uint _hashA) external payable {
         require(
-            proposedBet[_commitment].value == 0,
+            proposedBet[_hashA].value == 0,
             "there is already a bet on that commitment"
         );
         require(msg.value > 0, "you need to actually bet something");
 
-        proposedBet[_commitment].sideA = msg.sender;
-        proposedBet[_commitment].value = msg.value;
-        proposedBet[_commitment].placedAt = block.timestamp;
+        proposedBet[_hashA].sideA = msg.sender;
+        proposedBet[_hashA].value = msg.value;
+        proposedBet[_hashA].hashA = _hashA;
+        proposedBet[_hashA].placedAt = block.timestamp;
         // accepted is false by default
 
-        emit BetProposed(_commitment, msg.value);
+        emit BetProposed(_hashA, msg.value);
     } // function proposeBet
 
     // Called by sideB to continue
-    function acceptBet(uint _commitment, uint _random) external payable {
+    function acceptBet(uint _hashA, uint _hashB) external payable {
+        require(!proposedBet[_hashA].accepted, "Bet has already been accepted");
         require(
-            !proposedBet[_commitment].accepted,
-            "Bet has already been accepted"
-        );
-        require(
-            proposedBet[_commitment].sideA != address(0),
+            proposedBet[_hashA].sideA != address(0),
             "Nobody made that bet"
         );
         require(
-            msg.value == proposedBet[_commitment].value,
+            msg.value == proposedBet[_hashA].value,
             "Need to bet the same amount as sideA"
         );
 
-        acceptedBet[_commitment].sideB = msg.sender;
-        acceptedBet[_commitment].acceptedAt = block.timestamp;
-        acceptedBet[_commitment].randomB = _random;
-        proposedBet[_commitment].accepted = true;
+        acceptedBet[_hashA].sideB = msg.sender;
+        acceptedBet[_hashA].hashB = _hashB;
+        acceptedBet[_hashA].acceptedAt = block.timestamp;
+        proposedBet[_hashA].accepted = true;
 
-        emit BetAccepted(_commitment, proposedBet[_commitment].sideA);
+        emit BetAccepted(_hashA, proposedBet[_hashA].sideA);
     } // function acceptBet
 
+    function encode(uint value) public pure returns (uint) {
+        return uint(keccak256(abi.encodePacked(value)));
+    }
+
     // Called by sideA to reveal their random value and conclude the bet
-    function reveal(uint _random) external {
+    function reveal(uint _hashA, uint _random) external {
+        require(proposedBet[_hashA].accepted, "Bet has not been accepted yet");
+
+        //find proposed bet by sender address and hashA
+        if (proposedBet[_hashA].sideA == msg.sender) {
+            // reveal from proposed bet side
+
+            // check for fraud
+            if (proposedBet[_hashA].hashA != encode(_random)) {
+                // fraud from side A, side B wins by default
+                address payable _sideA = payable(proposedBet[_hashA].sideA);
+                address payable _sideB = payable(acceptedBet[_hashA].sideB);
+                _sideB.transfer(2 * proposedBet[_hashA].value);
+
+                emit FraudDetected(_hashA, _sideA);
+
+                emit BetSettled(
+                    _hashA,
+                    _sideB,
+                    _sideA,
+                    proposedBet[_hashA].value
+                );
+
+                delete proposedBet[_hashA];
+                delete acceptedBet[_hashA];
+                return;
+            }
+
+            proposedBet[_hashA].randomA = _random;
+            proposedBet[_hashA].reveal = true;
+
+            emit BetRevealed(_hashA, msg.sender);
+        } else if (acceptedBet[_hashA].sideB == msg.sender) {
+            //must be coming from the accepted side
+
+            // check for fraud
+            if (acceptedBet[_hashA].hashB != encode(_random)) {
+                // fraud from side B, side A wins by default
+                address payable _sideA = payable(proposedBet[_hashA].sideA);
+                address payable _sideB = payable(acceptedBet[_hashA].sideB);
+                _sideA.transfer(2 * proposedBet[_hashA].value);
+
+                emit FraudDetected(_hashA, _sideB);
+
+                emit BetSettled(
+                    _hashA,
+                    _sideA,
+                    _sideB,
+                    proposedBet[_hashA].value
+                );
+
+                delete proposedBet[_hashA];
+                delete acceptedBet[_hashA];
+                return;
+            }
+
+            acceptedBet[_hashA].randomB = _random;
+            acceptedBet[_hashA].reveal = true;
+
+            emit BetRevealed(_hashA, msg.sender);
+        } else {
+            revert("Could not find existing bet for sender address");
+        }
+
+        if (proposedBet[_hashA].reveal && acceptedBet[_hashA].reveal) {
+            // both sides have submitted real values, announce winner
+
+            address payable _sideA = payable(proposedBet[_hashA].sideA);
+            address payable _sideB = payable(acceptedBet[_hashA].sideB);
+            uint256 _randomA = proposedBet[_hashA].randomA;
+            uint256 _randomB = acceptedBet[_hashA].randomB;
+
+            uint256 _agreedRandom = _randomA ^ _randomB;
+            uint256 _value = proposedBet[_hashA].value;
+
+            if (_agreedRandom % 2 == 0) {
+                // side A wins
+                _sideA.transfer(2 * _value);
+                emit BetSettled(_hashA, _sideA, _sideB, _value);
+            } else {
+                _sideB.transfer(2 * _value);
+                emit BetSettled(_hashA, _sideB, _sideA, _value);
+            }
+
+            // Cleanup
+            delete proposedBet[_hashA];
+            delete acceptedBet[_hashA];
+        }
+
+        /*
         uint _commitment = uint256(keccak256(abi.encodePacked(_random)));
         address payable _sideA = payable(msg.sender);
         address payable _sideB = payable(acceptedBet[_commitment].sideB);
@@ -110,5 +210,6 @@ contract Casino {
         // Cleanup
         delete proposedBet[_commitment];
         delete acceptedBet[_commitment];
+        */
     } // function reveal
 } // contract Casino
--- test/casino-test.js
+++ test/casino-test.js
@@ -1,195 +1,222 @@
-const chai = require("chai")
-const expect = chai.expect
-chai.use(require('chai-as-promised'))
+const chai = require("chai");
+const expect = chai.expect;
+chai.use(require("chai-as-promised"));
 
-const valA = ethers.utils.keccak256(0xBAD060A7)
-const hashA = ethers.utils.keccak256(valA)
-const valBwin = ethers.utils.keccak256(0x600D60A7)
-const valBlose = ethers.utils.keccak256(0xBAD060A7)
+const trueA = 5;
+const trueB = 4;
+const trueBlose = 7;
+let hashA;
+let valBwin;
+let valBlose;
 
 // Chai's expect(<operation>).to.be.revertedWith behaves
 // strangely, so I'm implementing that functionality myself
 // with try/catch
-const interpretErr = err => {
-  if (err.reason)
-    return err.reason
-  else
-    return err.stackTrace[0].message.value.toString('ascii')
-}
+const interpretErr = (err) => {
+    if (err.reason) return err.reason;
+    else return err.stackTrace[0].message.value.toString("ascii");
+};
 
 describe("Casino", async () => {
-  it("Not allow you to propose a zero wei bet", async () => {
-    f = await ethers.getContractFactory("Casino")
-    c = await f.deploy()
+    let accountA, accountB;
+    before(async () => {
+        [accountA, accountB] = await ethers.getSigners();
 
-    try {
-      tx = await c.proposeBet(hashA)
-      rcpt = await tx.wait()
+        f = await ethers.getContractFactory("Casino");
+        c = await f.deploy();
 
-      // If we get here, it's a fail
-      expect("this").to.equal("fail")
-    } catch(err) {
-      expect(interpretErr(err)).to
-        .match(/you need to actually bet something/)
-    }
-  })   // it "Not allow you to bet zero wei"
+        hashA = await c.encode(trueA);
+        valBwin = await c.encode(trueB);
+        valBlose = await c.encode(trueBlose);
+    });
 
-  it("Not allow you to accept a bet that doesn't exist", async () => {
-    f = await ethers.getContractFactory("Casino")
-    c = await f.deploy()
+    it("Not allow you to propose a zero wei bet", async () => {
+        f = await ethers.getContractFactory("Casino");
+        c = await f.deploy();
 
-    try {
-      tx = await c.acceptBet(hashA, valBwin, {value: 10})
-      rcpt = await tx.wait()
-      expect("this").to.equal("fail")
-    } catch (err) {
-        expect(interpretErr(err)).to
-          .match(/Nobody made that bet/)
-    }
-  })   // it "Not allow you to accept a bet that doesn't exist" 
+        try {
+            tx = await c.proposeBet(hashA);
+            rcpt = await tx.wait();
 
-  it("Allow you to propose and accept bets", async () => {
-    f = await ethers.getContractFactory("Casino")
-    c = await f.deploy()
+            // If we get here, it's a fail
+            expect("this").to.equal("fail");
+        } catch (err) {
+            expect(interpretErr(err)).to.match(
+                /you need to actually bet something/
+            );
+        }
+    }); // it "Not allow you to bet zero wei"
 
-    tx = await c.proposeBet(hashA, {value: 10})
-    rcpt = await tx.wait()
-    expect(rcpt.events[0].event).to.equal("BetProposed")
-    tx = await c.acceptBet(hashA, valBwin, {value: 10})
-    rcpt = await tx.wait()
-    expect(rcpt.events[0].event).to.equal("BetAccepted")      
-  })   // it "Allow you to accept a bet"
+    it("Not allow you to accept a bet that doesn't exist", async () => {
+        f = await ethers.getContractFactory("Casino");
+        c = await f.deploy();
 
-  it("Not allow you to accept an already accepted bet", async () => {
-    f = await ethers.getContractFactory("Casino")
-    c = await f.deploy()
+        try {
+            tx = await c.connect(accountB).acceptBet(hashA, valBwin, {
+                value: 10,
+            });
+            rcpt = await tx.wait();
+            expect("this").to.equal("fail");
+        } catch (err) {
+            expect(interpretErr(err)).to.match(/Nobody made that bet/);
+        }
+    }); // it "Not allow you to accept a bet that doesn't exist"
 
-    tx = await c.proposeBet(hashA, {value: 10})
-    rcpt = await tx.wait()
-    expect(rcpt.events[0].event).to.equal("BetProposed")
-    tx = await c.acceptBet(hashA, valBwin, {value: 10})
-    rcpt = await tx.wait()
-    expect(rcpt.events[0].event).to.equal("BetAccepted")
-    try {
-      tx = await c.acceptBet(hashA, valBwin, {value: 10})
-      rcpt = await tx.wait()   
-      expect("this").to.equal("fail")      
-    } catch (err) {
-        expect(interpretErr(err)).to
-          .match(/Bet has already been accepted/)
-    }          
-  })   // it "Not allow you to accept an already accepted bet" 
+    it("Allow you to propose and accept bets", async () => {
+        f = await ethers.getContractFactory("Casino");
+        c = await f.deploy();
 
+        tx = await c.proposeBet(hashA, { value: 10 });
+        rcpt = await tx.wait();
+        expect(rcpt.events[0].event).to.equal("BetProposed");
+        tx = await c.connect(accountB).acceptBet(hashA, valBwin, { value: 10 });
+        rcpt = await tx.wait();
+        expect(rcpt.events[0].event).to.equal("BetAccepted");
+    }); // it "Allow you to accept a bet"
 
-  it("Not allow you to accept with the wrong amount", async () => {
-    f = await ethers.getContractFactory("Casino")
-    c = await f.deploy()
+    it("Not allow you to accept an already accepted bet", async () => {
+        f = await ethers.getContractFactory("Casino");
+        c = await f.deploy();
 
-    tx = await c.proposeBet(hashA, {value: 10})
-    rcpt = await tx.wait()
-    expect(rcpt.events[0].event).to.equal("BetProposed")
-    try {
-      tx = await c.acceptBet(hashA, valBwin, {value: 11})
-      rcpt = await tx.wait()   
-      expect("this").to.equal("fail")      
-    } catch (err) {
-        expect(interpretErr(err)).to
-          .match(/Need to bet the same amount as sideA/)
-    }          
-  })   // it "Not allow you to accept with the wrong amount"   
+        tx = await c.proposeBet(hashA, { value: 10 });
+        rcpt = await tx.wait();
+        expect(rcpt.events[0].event).to.equal("BetProposed");
+        tx = await c.connect(accountB).acceptBet(hashA, valBwin, { value: 10 });
+        rcpt = await tx.wait();
+        expect(rcpt.events[0].event).to.equal("BetAccepted");
+        try {
+            tx = await c
+                .connect(accountB)
+                .acceptBet(hashA, valBwin, { value: 10 });
+            rcpt = await tx.wait();
+            expect("this").to.equal("fail");
+        } catch (err) {
+            expect(interpretErr(err)).to.match(/Bet has already been accepted/);
+        }
+    }); // it "Not allow you to accept an already accepted bet"
 
+    it("Not allow you to accept with the wrong amount", async () => {
+        f = await ethers.getContractFactory("Casino");
+        c = await f.deploy();
 
-  it("Not allow you to reveal with wrong value", async () => {
-    f = await ethers.getContractFactory("Casino")
-    c = await f.deploy()
+        tx = await c.proposeBet(hashA, { value: 10 });
+        rcpt = await tx.wait();
+        expect(rcpt.events[0].event).to.equal("BetProposed");
+        try {
+            tx = await c
+                .connect(accountB)
+                .acceptBet(hashA, valBwin, { value: 11 });
+            rcpt = await tx.wait();
+            expect("this").to.equal("fail");
+        } catch (err) {
+            expect(interpretErr(err)).to.match(
+                /Need to bet the same amount as sideA/
+            );
+        }
+    }); // it "Not allow you to accept with the wrong amount"
 
-    tx = await c.proposeBet(hashA, {value: 10})
-    rcpt = await tx.wait()
-    expect(rcpt.events[0].event).to.equal("BetProposed")
-    tx = await c.acceptBet(hashA, valBwin, {value: 10})
-    rcpt = await tx.wait()
-    expect(rcpt.events[0].event).to.equal("BetAccepted")
-    try {
-      tx = await c.reveal(valBwin)
-      rcpt = await tx.wait()   
-      expect("this").to.equal("fail")      
-    } catch (err) {
-        expect(interpretErr(err)).to
-          .match(/Not a bet you placed or wrong value/)
-    }          
-  })   // it "Not allow you to accept an already accepted bet" 
+    it("Not allow you to reveal with wrong value, side B automatically wins", async () => {
+        f = await ethers.getContractFactory("Casino");
+        c = await f.deploy();
 
+        tx = await c.proposeBet(hashA, { value: 10 });
+        rcpt = await tx.wait();
+        expect(rcpt.events[0].event).to.equal("BetProposed");
+        tx = await c.connect(accountB).acceptBet(hashA, valBwin, { value: 10 });
+        rcpt = await tx.wait();
+        expect(rcpt.events[0].event).to.equal("BetAccepted");
+        try {
+            tx = await c.reveal(hashA, 6);
+            rcpt = await tx.wait();
+            expect(rcpt.events[0].event).to.equal("FraudDetected");
+            expect(rcpt.events[1].event).to.equal("BetSettled");
+            let [commitment, winner, loser, value] = rcpt.events[1].args;
+            expect(winner).to.equal(accountB.address);
+        } catch (err) {
+            expect(interpretErr(err)).to.match(
+                /Not a bet you placed or wrong value/
+            );
+        }
+    }); // it "Not allow you to accept an already accepted bet"
 
-  it("Not allow you to reveal before bet is accepted", async () => {
-    f = await ethers.getContractFactory("Casino")
-    c = await f.deploy()
+    it("Not allow you to reveal before bet is accepted", async () => {
+        f = await ethers.getContractFactory("Casino");
+        c = await f.deploy();
 
-    tx = await c.proposeBet(hashA, {value: 10})
-    rcpt = await tx.wait()
-    expect(rcpt.events[0].event).to.equal("BetProposed")
-    try {
-      tx = await c.reveal(valA)
-      rcpt = await tx.wait()   
-      expect("this").to.equal("fail")      
-    } catch (err) {
-        expect(interpretErr(err)).to
-          .match(/Bet has not been accepted yet/)
-    }          
-  })   // it "Not allow you to reveal before bet is accepted"  
+        tx = await c.proposeBet(hashA, { value: 10 });
+        rcpt = await tx.wait();
+        expect(rcpt.events[0].event).to.equal("BetProposed");
+        try {
+            tx = await c.reveal(hashA, trueA);
+            rcpt = await tx.wait();
+            expect("this").to.equal("fail");
+        } catch (err) {
+            expect(interpretErr(err)).to.match(/Bet has not been accepted yet/);
+        }
+    }); // it "Not allow you to reveal before bet is accepted"
 
-  it("Work all the way through (B wins)", async () => {
-    signer = await ethers.getSigners()
-    f = await ethers.getContractFactory("Casino")
-    cA = await f.deploy()
-    cB = cA.connect(signer[1])
+    it("Work all the way through (B wins)", async () => {
+        signer = await ethers.getSigners();
+        f = await ethers.getContractFactory("Casino");
+        cA = await f.deploy();
+        cB = cA.connect(accountB);
 
-    tx = await cA.proposeBet(hashA, {value: 1e10})
-    rcpt = await tx.wait()
-    expect(rcpt.events[0].event).to.equal("BetProposed")
+        tx = await cA.proposeBet(hashA, { value: 1e14 });
+        rcpt = await tx.wait();
+        expect(rcpt.events[0].event).to.equal("BetProposed");
 
-    tx = await cB.acceptBet(hashA, valBwin, {value: 1e10})
-    rcpt = await tx.wait()
-    expect(rcpt.events[0].event).to.equal("BetAccepted")      
+        tx = await cB.acceptBet(hashA, valBwin, { value: 1e14 });
+        rcpt = await tx.wait();
+        expect(rcpt.events[0].event).to.equal("BetAccepted");
 
-    // A sends the transaction, so the change due to the
-    // bet will only be clearly visible in B
+        // A sends the transaction, so the change due to the
+        // bet will only be clearly visible in B
 
-    preBalanceB = await ethers.provider.getBalance(signer[1].address)    
-    tx = await cA.reveal(valA)
-    rcpt = await tx.wait()
-    expect(rcpt.events[0].event).to.equal("BetSettled")
-    postBalanceB = await ethers.provider.getBalance(signer[1].address)        
-    deltaB = postBalanceB.sub(preBalanceB)
-    expect(deltaB.toNumber()).to.equal(2e10)   
+        preBalanceB = await ethers.provider.getBalance(accountB.address);
 
+        tx = await cA.reveal(hashA, trueA);
+        rcpt = await tx.wait();
+        expect(rcpt.events[0].event).to.equal("BetRevealed");
 
-  })   // it "Work all the way through (B wins)"
+        tx = await cB.reveal(hashA, trueB);
+        rcpt = await tx.wait();
+        expect(rcpt.events[0].event).to.equal("BetRevealed");
+        expect(rcpt.events[1].event).to.equal("BetSettled");
 
+        postBalanceB = await ethers.provider.getBalance(accountB.address);
 
-  it("Work all the way through (A wins)", async () => {
-    signer = await ethers.getSigners()
-    f = await ethers.getContractFactory("Casino")
-    cA = await f.deploy()
-    cB = cA.connect(signer[1])
+        deltaB = postBalanceB.sub(preBalanceB);
+        expect(deltaB.toNumber()).gt(0);
+    }); // it "Work all the way through (B wins)"
 
-    tx = await cA.proposeBet(hashA, {value: 1e10})
-    rcpt = await tx.wait()
-    expect(rcpt.events[0].event).to.equal("BetProposed")
+    it("Work all the way through (A wins)", async () => {
+        signer = await ethers.getSigners();
+        f = await ethers.getContractFactory("Casino");
+        cA = await f.deploy();
+        cB = cA.connect(accountB);
 
-    tx = await cB.acceptBet(hashA, valBlose, {value: 1e10})
-    rcpt = await tx.wait()
-    expect(rcpt.events[0].event).to.equal("BetAccepted")      
+        tx = await cA.proposeBet(hashA, { value: 1e14 });
+        rcpt = await tx.wait();
+        expect(rcpt.events[0].event).to.equal("BetProposed");
 
-    // A sends the transaction, so the change due to the
-    // bet will only be clearly visible in B
-    preBalanceB = await ethers.provider.getBalance(signer[1].address)    
-    tx = await cA.reveal(valA)
-    rcpt = await tx.wait()
-    expect(rcpt.events[0].event).to.equal("BetSettled")
-    postBalanceB = await ethers.provider.getBalance(signer[1].address)        
-    deltaB = postBalanceB.sub(preBalanceB)
-    expect(deltaB.toNumber()).to.equal(0) 
-  })   // it "Work all the way through (A wins)"
+        tx = await cB.acceptBet(hashA, valBlose, { value: 1e14 });
+        rcpt = await tx.wait();
+        expect(rcpt.events[0].event).to.equal("BetAccepted");
 
+        // A sends the transaction, so the change due to the
+        // bet will only be clearly visible in B
+        preBalanceB = await ethers.provider.getBalance(accountB.address);
+        tx = await cA.reveal(hashA, trueA);
+        rcpt = await tx.wait();
+        expect(rcpt.events[0].event).to.equal("BetRevealed");
 
-})     // describe("Casino")
+        tx = await cB.reveal(hashA, trueBlose);
+        rcpt = await tx.wait();
+        expect(rcpt.events[0].event).to.equal("BetRevealed");
+        expect(rcpt.events[1].event).to.equal("BetSettled");
+        postBalanceB = await ethers.provider.getBalance(accountB.address);
+        deltaB = postBalanceB.sub(preBalanceB);
+
+        expect(deltaB.toNumber()).lt(0);
+    }); // it "Work all the way through (A wins)"
+}); // describe("Casino")
